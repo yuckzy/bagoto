aab55c18a34c80fdf80dfa10696c95516e991bbd
yuckzy <delacruzdwyaneallen@gmail.com>
2025-04-06 08:28:06 +0000

    pili suki

---
+++ selectionsort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+    
+    cout << "Original array: ";
+    printArray(arr);
+    
+    selectionSort(arr);
+    
+    cout << "Sorted array: ";
+    printArray(arr);
+    
+    return 0;
+}

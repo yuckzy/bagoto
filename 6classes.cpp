ed666d3d823d2fe7e633ba9213b831aade696f7f
yuckzy <delacruzdwyaneallen@gmail.com>
2025-04-06 08:41:36 +0000

    7th example

---
+++ 6classes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+   public:
+      void setWidth(int w) {
+         width = w;
+      }
+      void setHeight(int h) {
+         height = h;
+      }
+      
+   protected:
+      int width;
+      int height;
+};
+
+class Rectangle: public Shape {
+   public:
+      int getArea();
+};
+
+Rectangle::getArea() { 
+return width * height;
+}
+
+int main(void) {
+   Rectangle Rect;
+   Rect.setWidth(5);
+   Rect.setHeight(7);
+   cout << "Total area: " << Rect.getArea() << endl;
+   system("pause");
+}

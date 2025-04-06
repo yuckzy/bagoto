f74f94e7f54757125fa3e2bbc66d6c9c12aba7a5
yuckzy <delacruzdwyaneallen@gmail.com>
2025-04-06 08:38:32 +0000

    2nd example

---
+++ 1classes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int width, height;
+  public:
+    Rectangle (int,int);
+    int area () {return (width*height);}
+};
+
+Rectangle::Rectangle (int a, int b) {
+  width = a;
+  height = b;
+}
+
+int main () {
+  Rectangle rectA (3,4);
+  Rectangle rectB (5,6);
+  
+  cout << "Area of Rectangle A: " << rectA.area() << endl;
+  cout << "Area of Rectangle B: " << rectB.area() << endl;
+  system("pause");
+}
